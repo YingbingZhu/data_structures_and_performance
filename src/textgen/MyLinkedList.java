16bc40b3e7309b6af9daf5899657b27a280dd16d
YingbingZhu <yingbing.zhu@hotmail.com>
2022-09-15 17:44:29 +0800

    Initial commit

---
+++ src/textgen/MyLinkedList.java
@@ -0,0 +1,132 @@
+package textgen;
+
+import org.reactfx.util.LL;
+
+import java.util.AbstractList;
+
+
+/** A class that implements a doubly linked list
+ * 
+ * @author UC San Diego Intermediate Programming MOOC team
+ *
+ * @param <E> The type of the elements stored in the list
+ */
+public class MyLinkedList<E> extends AbstractList<E> {
+	LLNode<E> head;
+	LLNode<E> tail;
+	int size;
+
+	/** Create a new empty LinkedList */
+	public MyLinkedList() {
+		this.head = new LLNode<>(null);
+		this.tail = new LLNode<>(null);
+		this.head.next = this.tail;
+		this.tail.prev = this.head;
+		this.size = 0;
+	}
+
+	/**
+	 * Appends an element to the end of the list
+	 * @param element The element to add
+	 */
+	public boolean add(E element ) 
+	{
+		if (element == null) {
+			throw new NullPointerException("invalid element");
+		}
+		LLNode<E> newNode = new LLNode<>(element);
+		LLNode<E> oldLast = tail.prev;
+
+		newNode.next = tail;
+		oldLast.next = newNode;
+		newNode.prev = oldLast;
+		tail.prev = newNode;
+		this.size ++;
+		return true;
+	}
+
+	/** Get the element at position index 
+	 * @throws IndexOutOfBoundsException if the index is out of bounds. */
+	public E get(int index) 
+	{
+		if (index < 0 || index > this.size - 1) {
+			throw new IndexOutOfBoundsException("invalid index");
+		}
+		LLNode<E> node = head;
+		for (int i = 0; i <= index; i++) {
+			node = node.next;
+		}
+		return node.data;
+	}
+
+	/**
+	 * Add an element to the list at the specified index
+	 * @param index where the element should be added
+	 * @param element The element to add
+	 */
+	public void add(int index, E element ) 
+	{
+		// TODO: Implement this method
+		// if the index provided is not reasonable for inserting an element
+		if (index < 0 || index > this.size - 1) {
+			throw new IndexOutOfBoundsException("invalid index");
+		}
+		// Null elements are not allowed
+		if (element == null) {
+			throw new NullPointerException("invalid element");
+		}
+		// If elements exist at that index, you will move elements at that index (and beyond) up,
+		// effectively inserting this element at that location in the list
+
+	}
+
+
+	/** Return the size of the list */
+	public int size() 
+	{
+		return this.size;
+	}
+
+	/** Remove a node at the specified index and return its data element.
+	 * @param index The index of the element to remove
+	 * @return The data element removed
+	 * @throws IndexOutOfBoundsException If index is outside the bounds of the list
+	 * 
+	 */
+	public E remove(int index) 
+	{
+		// TODO: Implement this method
+		return null;
+	}
+
+	/**
+	 * Set an index position in the list to a new element
+	 * @param index The index of the element to change
+	 * @param element The new element
+	 * @return The element that was replaced
+	 * @throws IndexOutOfBoundsException if the index is out of bounds.
+	 */
+	public E set(int index, E element) 
+	{
+		// TODO: Implement this method
+		return null;
+	}   
+}
+
+class LLNode<E> 
+{
+	LLNode<E> prev;
+	LLNode<E> next;
+	E data;
+
+	// TODO: Add any other methods you think are useful here
+	// E.g. you might want to add another constructor
+
+	public LLNode(E e) 
+	{
+		this.data = e;
+		this.prev = null;
+		this.next = null;
+	}
+
+}
